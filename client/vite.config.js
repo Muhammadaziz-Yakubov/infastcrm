e06e2d7a442ea767e663f00b1e18eddaf388c074
Muhammadaziz-Yakubov <muhammadazizyaqubov2@gmail.com>
2026-01-18 14:53:34 +0500

    message

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://infastcrm-0b2r.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path
       }
